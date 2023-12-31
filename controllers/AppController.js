9037a74549102f23f4017e0a260dfc3c42f0e6cc
ofentseloeto <ohentseloeto610@gmail.com>
2023-12-31 06:37:39 -0800

    fix

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,20 +1,28 @@
-import redisClient from '../utils/redis';
-import { countUsers, countFiles } from '../utils/db';
+import { isAlive as isRedisAlive, getCount as getUsersCount } from '../utils/redis';
+import { getCount as getFilesCount } from '../utils/db';
 
-class AppController {
-  static async getStatus(req, res) {
-    const redisAlive = redisClient.isAlive();
-    const dbAlive = true;
+const AppController = {
+  async getStatus(req, res) {
+    try {
+      const isRedis = await isRedisAlive();
+      const isDB = await getFilesCount();
+      res.status(200).json({ redis: isRedis, db: isDB });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
 
-    res.status(200).json({ redis: redisAlive, db: dbAlive });
-  }
-
-  static async getStats(req, res) {
-    const usersCount = await countUsers();
-    const filesCount = await countFiles();
-
-    res.status(200).json({ users: usersCount, files: filesCount });
-  }
-}
+  async getStats(req, res) {
+    try {
+      const usersCount = await getUsersCount();
+      const filesCount = await getFilesCount();
+      res.status(200).json({ users: usersCount, files: filesCount });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+};
 
 export default AppController;
